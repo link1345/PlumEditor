49fd84fa0c3d16dcf612a6d8aa385295a8022301
link <testkusa@gmail.com>
2023-06-25 10:50:52 +0900

    update #2

--- PlumEditor/main_menu.cpp
+++ PlumEditor/main_menu.cpp
@@ -3,6 +3,7 @@
 #include <Siv3D.hpp>
 #include <imgui.h>
 
+
 void Plum::MainMenu::Main(bool& isMainWindow) {
 	if (ImGui::BeginMainMenuBar())
 	{
@@ -34,9 +35,114 @@ void Plum::Plugin::MainWindow() {
 
 	if (!this->isMainWindow) return;
 
-	if(ImGui::Begin("test Plugin")) {
+	if(ImGui::Begin("Plugin List")) {
 
+		for (auto& item : this->plugin_list) {
+			if ( ImGui::CollapsingHeader(item.folder_path.filename().string().c_str()) )
+			{
+				for (auto& item_file : item.plugin_file) {
+
+					bool item_Click = item_file.isWindowOpen;
+
+					ImGui::Selectable( item_file.file_path.filename().string().c_str(), &item_Click);
+
+					if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(0))
+					{
+						// Do stuff on Selectable() double click.                                                                                                                                                                                                                           
+						item_file.isWindowOpen = !item_Click;
+					}
+
+					// 「今、開いた」場合、ロード処理を掛ける
+					if (item_file.isWindowOpen && item_file.isWindowOpen != item_Click) {
+						item_file.FileLoad(0,100);
+					}
+
+					// 「開いている」場合、表示する
+					if (item_file.isWindowOpen ) {
+						//ImGui::Text("hello!");
+
+						item_file.CodeWindow();
+
+
+					}
+				}
+			}
+		}
 	}
 	ImGui::End();
 
+}
+
+/*
+void Plum::Plugin::PluginWindow() {
+	for (auto& item : this->plugin_list) {
+
+		for (auto& item_file : item.plugin_file) {
+
+			if (ImGui::Begin( item_file.file_path.filename().string().c_str() )) {
+
+				
+
+			}
+			ImGui::End();
+
+		}
+
+	}
+}
+*/
+
+int Plum::PluginItem_file::FileLoad(int start, int end) {
+
+	this->code.clear();
+
+	std::ifstream ifs(this->file_path);
+
+	if (ifs.fail()) {
+		//std::cerr << "Failed to open file." << std::endl;
+		return -1;
+	}
+
+	if (start >= end) return -2;
+
+	this->start = start;
+	this->end = end;
+
+	int count = 0;
+
+	string tmp_code = "";
+
+	while (getline(ifs, tmp_code)) {
+		//std::cout << "#" << code << std::endl;
+
+		count++;
+
+		if (start > count) continue;
+		else if (end < count) break;
+
+		this->code.push_back(tmp_code);
+
+		if (ifs.eof()) {
+			this->end = count;
+			break;
+		}
+
+	}
+
+	ifs.close();
+
+}
+
+void Plum::PluginItem_file::CodeWindow() {
+
+	std::string window_name = "Code : " + this->file_path.filename().string();
+
+	if (ImGui::Begin(window_name.c_str())) {
+		for (auto& code_line : this->code) {
+
+			ImGui::Text(code_line.c_str());
+
+		}
+	}
+	ImGui::End();
 }
--- PlumEditor/main_menu.h
+++ PlumEditor/main_menu.h
@@ -1,5 +1,18 @@
 ﻿#pragma once
 
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <filesystem>
+#include <list>
+
+#include <fstream>
+
+using std::cout; using std::cin;
+using std::endl; using std::string;
+using std::filesystem::directory_iterator;
+
 namespace Plum {
 
 	class MainMenu {
@@ -7,14 +20,95 @@ namespace Plum {
 		void Main(bool &isMainWindow);
 	};
 
+	class PluginItem_file {
+	public :
+		PluginItem_file() {
+			this->isWindowOpen = false;
+
+			this->code = std::list<string>();
+
+		}
+
+		int FileLoad(int start , int end);
+
+		/// @brief プラグインのファイル
+		std::filesystem::path file_path;
+
+		/// @brief プラグインのウィンドウ開いているか？
+		bool isWindowOpen;
+
+		std::list<string> code;
+
+		int hasStart() {
+			return this->start;
+		}
+		int hasEnd() {
+			return this->end;
+		}
+
+		void CodeWindow();
+
+
+	private:
+		int start;
+		int end;
+	};
+
+	class PluginItem {
+	public :
+		PluginItem() {
+			plugin_file = std::list<PluginItem_file>();
+		}
+
+		/// @brief プラグインのフォルダ
+		std::filesystem::path folder_path;
+
+		/// @brief プラグインのファイル一覧
+		std::list<PluginItem_file> plugin_file;
+
+		// ここに何かconfigなどあったら追記する
+	};
+
 	class Plugin {
 	public :
 		Plugin() {
 			this->isMainWindow = false;
+
+			this->plugin_list = std::list<PluginItem>();
+
+			this->init_plugin();
 		}
 
+		/// @brief プラグインの一覧初期化
+		void init_plugin() {
+			plugin_list.clear();
+
+			for (const auto& pl_file : directory_iterator("./plugin/")) {
+
+				if (!pl_file.is_directory()) continue;
+
+				PluginItem item = PluginItem();
+				item.folder_path = pl_file.path();
+
+				for (const auto& file : directory_iterator(item.folder_path)) {
+					PluginItem_file item_file = PluginItem_file();
+					item_file.file_path = file;
+					item.plugin_file.push_back( item_file );
+				}
+
+				plugin_list.push_back(item);
+			}
+		}
+
+		/// @brief プラグインの一覧ウィンドウが開いているか？
 		bool isMainWindow;
 
+		/// @brief ウィンドウのプログラム
 		void MainWindow();
+
+		//void PluginWindow();
+
+		/// @brief プラグインのリスト
+		std::list<PluginItem> plugin_list;
 	};
 }
